f6368ddf75831a94b4b39e0420c04689bce57c1d
youssefdarahem <youssef.darahem@gmail.com>
2022-07-27 14:58:17 +0300

    Exercise A

--- bin/quiz1.dart
+++ bin/quiz1.dart
@@ -1,5 +1,10 @@
 import 'package:quiz1/quiz1.dart' as quiz1;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${quiz1.calculate()}!');
+  //Excersie A
+  print('Enter a year : ');
+  int year = int.parse(stdin.readLineSync()!);
+  bool isLeap = quiz1.isLeap(year);
+  isLeap ? print('$year is a Leap year') : print('$year is Not a Leap year!');
 }
--- lib/quiz1.dart
+++ lib/quiz1.dart
@@ -1,3 +1,9 @@
-int calculate() {
-  return 6 * 7;
+bool isLeap(int year) {
+  if (year % 400 == 0 && year % 100 == 0) {
+    return true;
+  } else if (year % 4 == 0 && year % 100 != 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
